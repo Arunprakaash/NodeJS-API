775b35eba43a298a76918028d5f0985eb97cbb2a
Arunprakaash <62831206+Arunprakaash@users.noreply.github.com>
2022-05-29 11:14:32 +0530

    Update connection.js

--- connection.js
+++ connection.js
@@ -1,11 +1,5 @@
 const {Client} = require('pg')
 
-const client = new Client({
-    host: "ec2-34-227-120-79.compute-1.amazonaws.com",
-    user: "uypuzhgtfzkuel",
-    port: 5432,
-    password: "6270900d1114cd9a7d8bf5ebc4eb2c37021d15c6d505f0dda8c09f6ca364a443",
-    database: "dciuejfjchcg8b"
-})
+const client = new Client('postgres://uypuzhgtfzkuel:6270900d1114cd9a7d8bf5ebc4eb2c37021d15c6d505f0dda8c09f6ca364a443@ec2-34-227-120-79.compute-1.amazonaws.com:5432/dciuejfjchcg8b')
 
 module.exports = client
