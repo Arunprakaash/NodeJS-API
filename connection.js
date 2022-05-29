188c0795706de750dbfbeb3259d3d871dc9916c2
Arunprakaash <62831206+Arunprakaash@users.noreply.github.com>
2022-05-29 13:16:12 +0530

    Update connection.js

--- connection.js
+++ connection.js
@@ -1,7 +1,7 @@
 const {Client} = require('pg')
 
 const client = new Client({
-    host: "localhost",
+    host: "ec2-34-227-120-79.compute-1.amazonaws.com",
     user: "uypuzhgtfzkuel",
     port: 5432,
     password: "6270900d1114cd9a7d8bf5ebc4eb2c37021d15c6d505f0dda8c09f6ca364a443",
