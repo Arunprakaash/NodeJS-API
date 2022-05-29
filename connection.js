a4bb7d1b5651467ce4f23e97a59bae12aae5ccbc
Arunprakaash <62831206+Arunprakaash@users.noreply.github.com>
2022-05-29 13:40:33 +0530

    Update connection.js

--- connection.js
+++ connection.js
@@ -5,7 +5,10 @@ const client = new Client({
     user: "uypuzhgtfzkuel",
     port: 5432,
     password: "6270900d1114cd9a7d8bf5ebc4eb2c37021d15c6d505f0dda8c09f6ca364a443",
-    database: "dciuejfjchcg8b"
+    database: "dciuejfjchcg8b",
+    ssl:{
+        rejectUnauthorized: false
+    }
 })
 
 module.exports = client
